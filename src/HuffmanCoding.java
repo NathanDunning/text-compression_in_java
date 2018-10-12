9372473619a7336de705045551a492d1f064527a
Nathan <Nathan@DESKTOP-Q2JVTB5>
2018-10-12 16:59:44 +1300

    Huffman Complete but not working properly

--- src/HuffmanCoding.java
+++ src/HuffmanCoding.java
@@ -1,3 +1,9 @@
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+import com.sun.prism.image.CompoundTexture;
+
 /**
  * A new instance of HuffmanCoding is created for every run. The constructor is
  * passed the full text to be encoded or decoded, so this is a good place to
@@ -5,11 +11,16 @@
  * the encode and decode methods.
  */
 public class HuffmanCoding {
-	/**
-	 * This would be a good place to compute and store the tree.
-	 */
+	
+	private HuffmanTree Tree;
+	private HashMap<Character, Integer> FreqMap;
+	private HashMap<Character, String> PrefixCodes;
+	
 	public HuffmanCoding(String text) {
-		// TODO fill this in.
+		this.FreqMap = getFreq(text.toCharArray());
+		this.Tree = buildHuffmanTree(FreqMap);
+		this.PrefixCodes = generatePrefixFreeCode(Tree);
+		
 	}
 
 	/**
@@ -18,8 +29,13 @@ public class HuffmanCoding {
 	 * only 1 and 0.
 	 */
 	public String encode(String text) {
-		// TODO fill this in.
-		return "";
+		//Using a char array StringBuilder because it runs faster than the string constructor
+		char[] characters = text.toCharArray();
+		StringBuilder binaryText = new StringBuilder();
+		for(char c : characters) {
+			binaryText.append(PrefixCodes.get(c));
+		}
+		return binaryText.toString();
 	}
 
 	/**
@@ -27,8 +43,34 @@ public class HuffmanCoding {
 	 * and return the decoded text as a text string.
 	 */
 	public String decode(String encoded) {
-		// TODO fill this in.
-		return "";
+		//Using a char array StringBuilder because it runs faster than the string constructor
+		char[] characters = encoded.toCharArray();
+		StringBuilder decodedText = new StringBuilder();
+		HuffmanNode root, pointer;
+		
+		//Traverse the tree in the order of the encoded string
+		pointer = root = Tree.getRoot();
+		for(int i=0; i<characters.length; i++) {
+			char textPointer = characters[i];
+			if(textPointer == '0') {
+				//Get the left node
+				pointer = pointer.getLeft();
+				if(pointer.getLeft() == null) {
+					//Add to string and back to root
+					decodedText.append(pointer.getChar());
+					pointer = root;
+				}
+			}
+			else if(textPointer == '1') {
+				//Get the right node
+				pointer = pointer.getRight();
+				if(pointer.getLeft() == null) {
+					decodedText.append(pointer.getChar());
+					pointer = root;
+				}
+			}
+		}
+		return decodedText.toString();
 	}
 
 	/**
@@ -39,5 +81,159 @@ public class HuffmanCoding {
 	 */
 	public String getInformation() {
 		return "";
+	}
+	
+	private class HuffmanTree {
+		private HuffmanNode root;
+		
+		public HuffmanTree(HuffmanNode root) {
+			this.root = root;
+		}
+		
+		public HuffmanNode getRoot() {
+			return this.root;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param ch array of char containing all the text
+	 * @return HashMap where all characters and values is a frequency
+	 */
+	private HashMap<Character, Integer> getFreq(char[] ch){
+		HashMap<Character, Integer> freq = new HashMap<Character, Integer>();
+		for(char c : ch) {
+			if (freq.containsKey(c)) {
+				freq.put(c, freq.get(c) + 1);
+			}
+			else {
+				freq.put(c, 1);
+			}
+		}
+		return freq;
+	}
+	
+	/**
+	 * 
+	 * @param freq -- frequency array of the text
+	 * @return HuffmanTree 
+	 */
+	private HuffmanTree buildHuffmanTree(HashMap<Character, Integer> freq) {
+		PriorityQueue<HuffmanNode> nodeQueue = new PriorityQueue<HuffmanNode>();
+		//adding the leaves into the priority queue
+		for(char c : freq.keySet()) {
+			HuffmanNode node = new HuffmanNode(c);
+			node.setFrequency(freq.get(c));
+			nodeQueue.offer(node);
+		}
+		
+		//Iterate through the leaves and build up
+		for(int i=1; i<nodeQueue.size(); i++) {
+			HuffmanNode huffNode1 = nodeQueue.poll();
+			HuffmanNode huffNode2 = nodeQueue.poll();
+			HuffmanNode parent = new HuffmanNode('\0');
+			parent.setFrequency(huffNode1.getFrequency() + huffNode2.getFrequency());
+			parent.setLeft(huffNode1);
+			parent.setRight(huffNode2);
+			huffNode1.setParent(parent);
+			huffNode2.setParent(parent);
+			nodeQueue.offer(parent);
+		}
+		
+		//Poll last node and return
+		return new HuffmanTree(nodeQueue.poll());
+	}
+
+	/**
+	 * Creates the prefix free code trees
+	 * @param hTree The huffman tree
+	 * @return 
+	 */
+	private HashMap<Character, String> generatePrefixFreeCode(HuffmanTree hTree){
+		//Traverse through the tree and assign each node a code
+		HashMap<Character, String> prefix = new HashMap<Character, String>();
+		Stack<HuffmanNode> hStack = new Stack<HuffmanNode>();
+		hStack.push(hTree.getRoot());
+		
+		while(!hStack.isEmpty()) {
+			HuffmanNode node = hStack.pop();
+			HuffmanNode left = node.getLeft();
+			HuffmanNode right = node.getRight();
+			
+			if(left != null) {
+				//Sets the prefix code of the children (left = parent0) and (right = parent1)
+				//Compression 1/2 P13
+				left.setPrefix(node.getPrefix() + '0');
+				hStack.push(left);
+				right.setPrefix(node.getPrefix() + '1');
+				hStack.push(right);
+			}
+			else {
+				prefix.put(node.getChar(), node.getPrefix());
+			}
+		}
+		return prefix;
+	}
+	
+	private class HuffmanNode implements Comparable<HuffmanNode>{
+		
+		private char character;
+		private int frequency;
+		private String prefix;
+		private HuffmanNode parent;
+		private HuffmanNode left;
+		private HuffmanNode right;
+		
+		public HuffmanNode(char c) {
+			this.character = c;
+			frequency = 0;
+			prefix = "";
+		}
+		
+		@Override
+		public int compareTo(HuffmanNode other) {
+			return this.frequency - other.frequency;
+		}
+		
+		public int getFrequency() {
+			return this.frequency;
+		}
+		
+		public void setFrequency(int f) {
+			this.frequency = f;
+		}
+		
+		public char getChar() {
+			return this.character;
+		}
+		
+		public void setParent(HuffmanNode parent) {
+			this.parent = parent;
+		}
+		
+		public void setLeft(HuffmanNode left) {
+			this.left = left;
+		}
+		
+		public HuffmanNode getLeft() {
+			return this.left;
+		}
+		
+		public void setRight(HuffmanNode right) {
+			this.right = right;
+		}
+		
+		public HuffmanNode getRight() {
+			return this.right;
+		}
+		
+		public void setPrefix(String s) {
+			this.prefix = s;
+		}
+		
+		public String getPrefix() {
+			return this.prefix;
+		}
+		
 	}
 }
